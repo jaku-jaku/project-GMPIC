c8bf512738f145ac60d22478b4fe4d46f768dde5
Jack <projectbyjx@gmail.com>
2023-07-28 13:36:14 -0400

    + verified body jacobian and body list + we can compute body jacobian from either body list or sparial list

--- Geometric-Impedence-MPC/main_model.m
+++ Geometric-Impedence-MPC/main_model.m
@@ -15,8 +15,8 @@ CLOSE_WINDOW = true; % pre-closing
 AUTO_CLOSE   = false;
 
 helper.createFolder("output/test", false);
-helper.setLogLevel("all")
-validate.set_validatorLevel("all")
+helper.setLogLevel("error")         % [ all, debug, error, info ]
+validate.set_validatorLevel("all")  % [ all, none ]
 % --- 
 helper.endSection(AUTO_CLOSE);
 %% Initial g(0)) ===== ===== ===== ===== ===== ===== =====:
@@ -39,17 +39,29 @@ N_links = length(common.WAM);
 N_jnts = N_links; % we cosider (i-1)===>(i)'s joints/frames
 valid_jnts_filter = eye(N_jnts); % <--- assume all joints active by default
 
+% [placeholder]:
+joint_limits_ = cell(1,N_links); 
+G_SE3_ = cell(1,N_links); 
+% spatial:
+xi_R6_s_ = cell(1,N_links); 
+G_SE3_s_ = cell(1,N_links);
+% moment:
+M_R6x6_ = cell(1,N_links); 
+Ad_inv_g0_s_l_ = cell(1,N_links); 
+M_R6x6_spatial_ = cell(1,N_links); 
+
+% [iterating links forward]:
 for i = 1:N_links
     helper.loginfo(sprintf("> Indexing Links @ %d-->%d",i-1,i));
     % 1. obtain relative params for current link (i-1)-->(i)
-    q_R3_i = common.WAM(i).tail_frame_transformation.q';
-    w_R3_i = common.WAM(i).tail_frame_transformation.w';
-    t_R_i  = common.WAM(i).tail_frame_transformation.t;
-    w_joint_axis_i = common.WAM(i).tail_frame_revolute_joint.axis';
+    q_R3_i = common.WAM(i).tip_frame_transformation.q';
+    w_R3_i = common.WAM(i).tip_frame_transformation.w';
+    t_R_i  = common.WAM(i).tip_frame_transformation.t;
+    w_joint_axis_i = common.WAM(i).tip_frame_revolute_joint.axis';
 
     % 2. output joint properties:
-    joint_name_i = common.WAM(i).tail_frame_revolute_joint.name;
-    joint_limits_{i} = common.WAM(i).tail_frame_revolute_joint.limits;
+    joint_name_i = common.WAM(i).tip_frame_revolute_joint.name;
+    joint_limits_{i} = common.WAM(i).tip_frame_revolute_joint.limits;
 
     % - store joint info:
     if joint_name_i == "NotActive"
@@ -63,57 +75,123 @@ for i = 1:N_links
     % 4. SE3 operation:
     % - propagate base frame TF (i-1) to tip frame (i) in current link:
     if i == 1
-        G_SE3_s_i = WAM_Spatial_0 * G_SE3_{i}; % <--- initial wam base frame
+        G_SE3_s_{i} = WAM_Spatial_0 * G_SE3_{i}; % <--- initial wam base joint tip frame (J1)
     else
-        G_SE3_s_i = G_SE3_s_{i-1} * G_SE3_{i}; % <--- Propagation
+        G_SE3_s_{i} = G_SE3_s_{i-1} * G_SE3_{i}; % <--- Propagation
     end
     % - obtain global representation of the joint rotation axis
-    w_R3_s_i = G_SE3_s_i(1:3,1:3) * w_joint_axis_i; 
+    w_R3_s_i = G_SE3_s_{i}(1:3,1:3) * w_joint_axis_i; 
     
     % - twist:
-    xi_R6_s_i = RBT.twist_R6_from_axis_point(w_R3_s_i, G_SE3_s_i(1:3,4));
-    helper.logdebug(helper.a2str("xi_R6_0_i",xi_R6_s_i));
+    xi_R6_s_{i} = RBT.twist_R6_from_axis_point(w_R3_s_i, G_SE3_s_{i}(1:3,4));
     
-    % - compute generalized inertia matrix:
-    m_R_i = common.WAM(i).mass;
-    mc_R3_i = common.WAM(i).tail_frame_mass_center';
-    I_mc_R3x3_i = common.WAM(i).Tail_frame_MoI_at_mass; % inertia at mass center , aligned with output frame axis
-    mc_R3_i_hat = Lie.hat_so3_from_R3(mc_R3_i);
-    % - inertia at mass center , aligned with output frame axis:
-    % M_mass_center_i = [
-    %     m_R_i * eye(3)  , zeros(3,3);
-    %     zeros(3,3)      , I_mc_R3x3_i;
-    % ];
-    % - translating inertia matrix from center-of-mass to the output frame:
-    M_R6x6_{i} = [
-        m_R_i * eye(3)       , -m_R_i*mc_R3_i_hat; ...
-        m_R_i * mc_R3_i_hat  , I_mc_R3x3_i - m_R_i * mc_R3_i_hat^2
-    ]; % [pg 288, ]
-
-    % - adjoint (0)
-    % Ad_inv_g0_s_l_{i} = Lie.Ad_SE3_from_SE3(RBT.inverse_SE3(G_SE3_s_i));
-    Ad_inv_g0_s_l_{i} = Lie.inv_Ad_SE3_from_SE3(G_SE3_s_i); % equivalent
-
-    % - Inertia of the ith link reflected into the base spatial frame:
-    % [4.28, Murray]
-    M_R6x6_spatial_{i} = Ad_inv_g0_s_l_{i}' * M_R6x6_{i} * Ad_inv_g0_s_l_{i}; 
-    
-    % (cache): % for joint 'i' (1...N) @ frame 'i' (0...N)
-    xi_R6_s_{i} = xi_R6_s_i;    % [ spatial twist ] 
-    G_SE3_s_{i} = G_SE3_s_i;  % [ spatial transformation ] 
-    helper.logdebug(helper.a2str("G_SE3_s_i",G_SE3_s_i));
+%     % [ spatial twist ] 
+%     helper.logdebug(helper.a2str("xi_R6_s_{i}",xi_R6_s_{i}));
+%     % [ spatial transformation ] 
+%     helper.logdebug(helper.a2str("G_SE3_s_{i}",G_SE3_s_{i}));
 end
 
+xi_R6_s = cat(2,xi_R6_s_{:})
 
-
-
-%% Compute Kinematics) ===== ===== ===== ===== ===== ===== =====:
-DIR = helper.declareSection("test", "compute", SAVE_CONSOLE, CLEAR_OUTPUT, CLOSE_WINDOW);
+%% [INIT Jnt Angles]:
 % --- 
+JOINT_ANGLE = zeros(1,N_jnts)';
+% JOINT_ANGLE = pi/4 * valid_jnts_filter * ones(1,N_jnts)';
+%JOINT_ANGLE(3) = 0.2;
 
-% Given Angles:
-JOINT_ANGLE = pi/4 * valid_jnts_filter * ones(1,N_jnts)';
-% JOINT_ANGLE =zeros(1,N_jnts)';
+%% [iterating links backward]:
+G_SE3_rvr_ = cell(1,N_links);
+G_SE3_b_ = cell(1,N_links);
+xi_R6_b_ = cell(1,N_links);
+
+G_TOOL_0 = eye(4);
+
+xi_R6_b_{N_links} = [0,0,0,0,0,1]';
+% todo: please make sure the produced link can provide same jacobian
+for i = N_links:-1:2
+    helper.loginfo(sprintf("> Indexing Links @ %d-->%d",i+1,i));
+    % 1. obtain relative params for current link (i)-->(i+1)
+    G_SE3_rvr_{i} = RBT.inverse_SE3(G_SE3_{i}); % relative RBT inversed
+    % - propagate base frame inverse TF (i+1) to tip frame (i) in current link:
+    if i == N_links
+        G_SE3_b_{i} =  G_SE3_rvr_{i} * G_TOOL_0;
+    else
+        G_SE3_b_{i} =  G_SE3_b_{i+1} * G_SE3_rvr_{i}; % <--- Propagation
+    end
+
+    % - obtain global representation of the joint rotation axis
+    w_joint_axis_i = common.WAM(i).tip_frame_revolute_joint.axis';
+    w_R3_b_i = G_SE3_b_{i}(1:3,1:3) * w_joint_axis_i; 
+    
+    % - twist:
+    xi_R6_b_{i-1} = RBT.twist_R6_from_axis_point(w_R3_b_i, G_SE3_b_{i}(1:3,4));
+
+    
+%     % - compute generalized inertia matrix: ---->O---x--->---
+%     m_R_i = common.WAM(i).mass;
+%     mc_R3_i = common.WAM(i).tip_frame_mass_center';
+%     I_mc_R3x3_i = common.WAM(i).tip_frame_MoI_at_mass; % inertia at mass center , aligned with output frame axis
+%     mc_R3_i_hat = Lie.hat_so3_from_R3(mc_R3_i);
+%     % - inertia at mass center , aligned with output frame axis:
+%     % M_mass_center_i = [
+%     %     m_R_i * eye(3)  , zeros(3,3);
+%     %     zeros(3,3)      , I_mc_R3x3_i;
+%     % ];
+%     % - translating inertia matrix from center-of-mass to the output frame:
+%     M_R6x6_{i} = [
+%         m_R_i * eye(3)       , -m_R_i*mc_R3_i_hat; ...
+%         m_R_i * mc_R3_i_hat  , I_mc_R3x3_i - m_R_i * mc_R3_i_hat^2
+%     ]; % [pg 288, ]
+% 
+%     % - adjoint (0)
+%     % Ad_inv_g0_s_l_{i} = Lie.Ad_SE3_from_SE3(RBT.inverse_SE3(G_SE3_s_i));
+%     Ad_inv_g0_s_l_{i} = Lie.inv_Ad_SE3_from_SE3(G_SE3_s_i); % equivalent
+% 
+%     % - Inertia of the ith link reflected into the base spatial frame:
+%     % [4.28, Murray]
+%     M_R6x6_spatial_{i} = Ad_inv_g0_s_l_{i}' * M_R6x6_{i} * Ad_inv_g0_s_l_{i}; 
+end
+
+xi_R6_b = cat(2,xi_R6_b_{:})
+% inv_ad_gst = Lie.inv_Ad_SE3_from_SE3(G_SE3_wam_spatial_ours);
+
+%% Body) ===== ===== ===== ===== ===== ===== =====:
+DIR = helper.declareSection("test", "body", SAVE_CONSOLE, CLEAR_OUTPUT, CLOSE_WINDOW);
+
+
+% --- 
+% Body FK:
+% --- 
+Blist = cat(2, xi_R6_b_{:});
+% G_SE3_wam_body_MR = OpenChainMR.body_forward_kinematics(G_SE3_b_{1}, Blist, JOINT_ANGLE)
+% G_SE3_wam_body_MR_2 = G_SE3_b_{1} * OpenChainMR.spatial_forward_kinematics(eye(4), Blist, JOINT_ANGLE) 
+% % [NOTE]: as expected, they are equivalent
+% validate.if_equivalent(G_SE3_wam_body_MR, G_SE3_wam_body_MR_2, "MR: spatial vs body function");
+% % equivalent ours:
+% exp_xi_theta_in_SE3_body_ = OpenChain.batch_screw_SE3_from_twist_angle_R6xR(xi_R6_b_, JOINT_ANGLE);
+% G_SE3_wam_body_MR_ours = OpenChain.compute_Body_FK(exp_xi_theta_in_SE3_body_,G_SE3_b_{1})
+% % Test:
+% validate.if_equivalent(G_SE3_wam_body_MR, G_SE3_wam_body_MR_ours, "MR vs our function");
+% % more:
+% G_SE3_wam_body_MR_ours_ = OpenChain.compute_Body_FK_for_all_joints(exp_xi_theta_in_SE3_body_, G_SE3_b_);
+% % Test:
+% validate.if_equivalent(G_SE3_wam_body_MR_ours_{1}, G_SE3_wam_body_MR_ours, "our batch body function");
+
+% --- 
+% Body Jacobian:
+% --- 
+% MR:
+J_wam_body_MR = OpenChainMR.body_jacobian(Blist, JOINT_ANGLE)
+% ours:
+neg_exp_xi_theta_in_SE3_body_ = OpenChain.batch_screw_SE3_from_twist_angle_R6xR(xi_R6_b_, - JOINT_ANGLE);
+J_wam_body_ours = OpenChain.compute_Body_Jacobian(xi_R6_b_, neg_exp_xi_theta_in_SE3_body_)
+% Test:
+validate.if_equivalent(J_wam_body_MR, J_wam_body_ours, "compute_Body_Jacobian");
+
+
+%% Spatial) ===== ===== ===== ===== ===== ===== =====:
+DIR = helper.declareSection("test", "spatial", SAVE_CONSOLE, CLEAR_OUTPUT, CLOSE_WINDOW);
+% --- 
 
 % --- 
 % spacial FK:
@@ -125,29 +203,29 @@ G_SE3_wam_spatial_MR = OpenChainMR.spatial_forward_kinematics(G_SE3_s_{end}, Sli
 exp_xi_theta_in_SE3_ = OpenChain.batch_screw_SE3_from_twist_angle_R6xR(xi_R6_s_, JOINT_ANGLE);
 G_SE3_wam_spatial_ours = OpenChain.compute_Spatial_FK(exp_xi_theta_in_SE3_, G_SE3_s_{end});
 % check:
-validate.test_compare(G_SE3_wam_spatial_MR, G_SE3_wam_spatial_ours, "Spatial Forward Kinematics");
+validate.if_equivalent(G_SE3_wam_spatial_MR, G_SE3_wam_spatial_ours, "Spatial Forward Kinematics");
 % compute all joints FK:
 G_SE3_wam_spatial_ours_ = OpenChain.compute_Spatial_FK_for_all_joints(exp_xi_theta_in_SE3_, G_SE3_s_);
 
-
-%% --- 
+% --- 
 % spacial jacobian:
 % --- 
 % MR:
 J_wam_spatial_MR = OpenChainMR.spatial_jacobian(Slist, JOINT_ANGLE);
 % ours:
 J_wam_spatial_ours = OpenChain.compute_Spatial_Jacobian(xi_R6_s_, exp_xi_theta_in_SE3_);
-validate.test_compare(J_wam_spatial_MR, J_wam_spatial_ours, "Spatial Jacobian")
+validate.if_equivalent(J_wam_spatial_MR, J_wam_spatial_ours, "Spatial Jacobian")
 
+% --- 
+% body jacobian:
+% --- 
+ad_gst = Lie.Ad_SE3_from_SE3(G_SE3_wam_spatial_ours);
+% ours w/ spatial:
+J_wam_body_ours_v2 = OpenChain.compute_Body_Jacobian_from_Spatial(xi_R6_s_, exp_xi_theta_in_SE3_, G_SE3_s_{end})
+J_wam_spatial_from_body = ad_gst * J_wam_body_ours_v2; % [Murray 3.56]
+validate.if_equivalent(J_wam_spatial_from_body, J_wam_spatial_ours, "J_spatial = Ad_gst * J_body [Murray 3.56]")
 
-% % --- 
-% % body FK:
-% % --- 
-% G_SE3_wam_body_MR = OpenChainMR.body_forward_kinematics(G_SE3_s_{end}, Slist, JOINT_ANGLE);
-
-
-
-%% ---
+validate.if_equivalent(J_wam_body_ours_v2, J_wam_body_ours, "J_body_from_spatial = J_body")
 
 
 
@@ -160,20 +238,21 @@ figure(1)
 % tiledlayout(2,1);
 ax_1_1 = nexttile();
 
+% [ Spatial ]
 % plot summit:
 utils.plot_Summit(SUMMIT_POSE_SE3,SUMMIT_POSE_SE3,'S',ax_1_1);
 
 % plot base link:
 utils.plot_link( ...
     WAM_Spatial_0, ...
-    common.WAM(1).tail_frame_transformation.q', ...
+    common.WAM(1).tip_frame_transformation.q', ...
     common.WAM(1).base_frame, ...
     'k','--');
 % plot joint links:
 for i=2:N_links
     utils.plot_link( ...
         G_SE3_wam_spatial_ours_{i-1}, ...
-        common.WAM(i).tail_frame_transformation.q', ...
+        common.WAM(i).tip_frame_transformation.q', ...
         common.WAM(i).base_frame, ...
         'k','-.');
 end
@@ -181,8 +260,18 @@ end
 utils.plot_link( ...
     G_SE3_wam_spatial_ours_{N_links}, ...
     [0,0,0]', ...
-    common.WAM(N_links).tail_frame, ...
+    common.WAM(N_links).tip_frame, ...
     'k','-');
+
+% [ Body ]
+% % plot joint links:
+% for i=N_links:-1:N_links-1
+%     utils.plot_link( ...
+%         G_SE3_wam_spatial_ours_{i-1}, ...
+%         common.WAM(i).tip_frame_transformation.q', ...
+%         common.WAM(i).base_frame, ...
+%         'k','-.');
+% end
 
 grid on;
 axis equal;
--- Geometric-Impedence-MPC/utils/OpenChain.m
+++ Geometric-Impedence-MPC/utils/OpenChain.m
@@ -15,25 +15,58 @@ classdef OpenChain
 %                 exp_xi_theta_in_SE3_1_{i} = RBT.screw_SE3_from_axis_point_theta(w_R3_0_{i}, q_R3_0_{i}, theta_(i));
             end
         end
+        function G_st_SE3_1_ = compute_FWD_Exponential_Chain(exp_xi_theta_in_SE3_)
+            N_jnts = length(exp_xi_theta_in_SE3_);
+            G_st_SE3_1_ = cell(1, N_jnts); % -> (placeholder)
+            G_st_SE3_1_{1} = exp_xi_theta_in_SE3_{1};
+            for i=2:N_jnts
+                G_st_SE3_1_{i} = G_st_SE3_1_{i-1} * exp_xi_theta_in_SE3_{i};
+            end
+        end
+        function G_st_SE3_n_ = compute_RVR_Exponential_Chain(exp_xi_theta_in_SE3_)
+            N_jnts = length(exp_xi_theta_in_SE3_);
+            G_st_SE3_n_ = cell(1, N_jnts); % -> (placeholder)
+            G_st_SE3_n_{N_jnts} = exp_xi_theta_in_SE3_{N_jnts};
+            for i=N_jnts-1:-1:1
+                G_st_SE3_n_{i} = exp_xi_theta_in_SE3_{i} * G_st_SE3_n_{i+1};
+            end
+        end
+        %% % [ Cell Based Batch Process ] 
+        function cell_ = cell_prod(A,B)
+            [ AN, AM ] = size(A);
+            cell_ = cell(AN,AM);
+            for i = 1:AN
+                for j = 1:AM
+                    cell_{i,j} = A{i,j} * B{i,j};
+                end
+            end
+        end
         %% % [ Spatial ] %%% %%% %%% %%% %%% %%% %%%
         function G_s_st_SE3 = compute_Spatial_FK(exp_xi_theta_in_SE3_1_, g0_st_SE3)
             % @G0_SE3_st_EE: zero/home configuration of the ToolFrame: g_st(0)
             N_jnts = length(exp_xi_theta_in_SE3_1_);
             % > g_st(t) = exp_xi_1(t_1) * exp_xi_2(t_2) * ... * exp_xi_n(t_n) * g_st(0)
             G_s_st_SE3 = eye(4); 
-            
             for i=1:N_jnts
                 G_s_st_SE3 = G_s_st_SE3 * exp_xi_theta_in_SE3_1_{i};
             end
             G_s_st_SE3 = G_s_st_SE3 * g0_st_SE3; % apply zero config at EE joint
         end
         function G_s_st_SE3_ = compute_Spatial_FK_for_all_joints(exp_xi_theta_in_SE3_1_, g0_st_SE3_)
+%             % [ NEW IMPLICIT IMPLEMENTATION ]
+%             % @g0_st_SE3_: zero/home configuration for a series of joints: g_st_i(0)
+%             % > g_st(t) = exp_xi_1(t_1) * exp_xi_2(t_2) * ... * exp_xi_n(t_n) * g_st(0)
+%             g_st_SE3 = OpenChain.compute_FWD_Exponential_Chain(exp_xi_theta_in_SE3_1_);
+%             G_s_st_SE3_ = OpenChain.cell_prod(g_st_SE3, g0_st_SE3_);
+            
+            % [ ORIGINAL EXPLICIT IMPLEMENTATION ]
             % @g0_st_SE3_: zero/home configuration for a series of joints: g_st_i(0)
             N_jnts = length(exp_xi_theta_in_SE3_1_);
             validate.if_dimension("g0_st_SE3_", g0_st_SE3_, [1 N_jnts]);
             G_s_st_SE3_ = cell(1, N_jnts); % -> (placeholder)
             % > g_st(t) = exp_xi_1(t_1) * exp_xi_2(t_2) * ... * exp_xi_n(t_n) * g_st(0)
             g_st_SE3 = eye(4); 
+            
             for i=1:N_jnts
                 g_st_SE3 = g_st_SE3 * exp_xi_theta_in_SE3_1_{i};
                 G_s_st_SE3_{i} = g_st_SE3 * g0_st_SE3_{i}; % apply zero config at each joint
@@ -43,10 +76,10 @@ classdef OpenChain
             N_jnts = length(xi_R6_1_);
             
             % J = [xi_1, xi_2', ... , xi_N' ]:
-            J_s_st_ = cell(1,N_jnts);
+            J_s_st = zeros(6,N_jnts);
             
             % [xi_1]:
-            J_s_st_{1} = xi_R6_1_{1};
+            J_s_st(:,1) = xi_R6_1_{1};
             g_st_SE3_1_i = eye(4); % init
             
             % [xi_2' ... xi_N']:
@@ -57,23 +90,65 @@ classdef OpenChain
                 % compute adjoint for 1 ... i-1:
                 Ad_1_i = Lie.Ad_SE3_from_SE3(g_st_SE3_1_i);
                 % compute jacobian @ xi_i:
-                J_s_st_{i} =  Ad_1_i * xi_R6_1_{i};
+                J_s_st(:,i) =  Ad_1_i * xi_R6_1_{i};
             end
-            
-            % [ form a 6x6 matrix ]
-            J_s_st = cat(2,J_s_st_{:}); 
         end
         %% % [ Body ] %%% %%% %%% %%% %%% %%% %%%
-        function G_s_st_SE3 = compute_Body_FK(exp_xi_theta_in_SE3_1_, g0_st_SE3)
+        function G_b_st_SE3 = compute_Body_FK(exp_xi_theta_in_SE3_body_, g0_st_SE3_body_)
             % @G0_SE3_st_EE: zero/home configuration of the ToolFrame: g_st(0)
-            N_jnts = length(exp_xi_theta_in_SE3_1_);
+            N_jnts = length(exp_xi_theta_in_SE3_body_);
             % > g_st(t) = exp_xi_1(t_1) * exp_xi_2(t_2) * ... * exp_xi_n(t_n) * g_st(0)
-            G_s_st_SE3 = eye(4); 
-            
+            G_b_st_SE3 = g0_st_SE3_body_; 
             for i=1:N_jnts
-                G_s_st_SE3 = G_s_st_SE3 * exp_xi_theta_in_SE3_1_{i};
+                G_b_st_SE3 = G_b_st_SE3 * exp_xi_theta_in_SE3_body_{i};
             end
-            G_s_st_SE3 = G_s_st_SE3 * g0_st_SE3;
+        end
+        function G_s_st_SE3_ = compute_Body_FK_for_all_joints(exp_xi_theta_in_SE3_body_, g0_st_SE3_body_)
+            % [ NEW IMPLICIT IMPLEMENTATION ]
+            % @g0_st_SE3_: zero/home configuration for a series of joints: g_st_i(0)
+            % > g_st(t) = exp_xi_1(t_1) * exp_xi_2(t_2) * ... * exp_xi_n(t_n) * g_st(0)
+            g_st_SE3 = OpenChain.compute_RVR_Exponential_Chain(exp_xi_theta_in_SE3_body_);
+            G_s_st_SE3_ = OpenChain.cell_prod(g0_st_SE3_body_, g_st_SE3);
+        end   
+        function J_b_st = compute_Body_Jacobian(xi_R6_body_, neg_exp_xi_theta_in_SE3_body_)
+            %{
+                Expecting:
+                    neg_exp_xi_theta_in_SE3_body_ = OpenChain.batch_screw_SE3_from_twist_angle_R6xR(xi_R6_b_, - JOINT_ANGLE);
+            %}
+            N_jnts = length(xi_R6_body_);
+            % J = [xi_1#, xi_2#, ... , xi_N# ]:
+            J_b_st = zeros(6,N_jnts);        
+
+            % [xi_N]:
+            J_b_st(:,N_jnts) = xi_R6_body_{N_jnts};
+            g_st_SE3_n_i = eye(4); % init
+            
+            % [xi_N-1' ... xi_1']:
+            for i=N_jnts - 1: -1: 1
+                % -> compute current jacobian:
+                % compute transformation for 1 ... i-1:
+                g_st_SE3_n_i = g_st_SE3_n_i * neg_exp_xi_theta_in_SE3_body_{i+1}; 
+                % compute adjoint for N ... i:
+                Ad_1_i = Lie.Ad_SE3_from_SE3(g_st_SE3_n_i);
+                % compute jacobian @ xi_i:
+                J_b_st(:,i) =  Ad_1_i * xi_R6_body_{i};
+            end
+        end
+        function J_b_st = compute_Body_Jacobian_from_Spatial(xi_R6_1_, exp_xi_theta_in_SE3_1_, g0_st_SE3_)
+            N_jnts = length(xi_R6_1_);
+            
+            % J = [xi_1, xi_2', ... , xi_N' ]:
+            J_b_st = zeros(6,N_jnts);
+            g_st_SE3_i_n = eye(4);
+            
+            % [xi_N' ... xi_1']:
+            for i=N_jnts:-1:1
+                % -> compute current jacobian:
+                g_st_SE3_i_n = exp_xi_theta_in_SE3_1_{i} * g_st_SE3_i_n; 
+                Ad_i_n = Lie.inv_Ad_SE3_from_SE3(g_st_SE3_i_n * g0_st_SE3_);
+                % compute jacobian @ xi_i:
+                J_b_st(:,i) =  Ad_i_n * xi_R6_1_{i};
+            end
         end
         %% Shortcuts:
         function g_st_SE3 = compute_Spatial_FK_from_theta(xi_R6_1_, theta_, g0_st_SE3)
