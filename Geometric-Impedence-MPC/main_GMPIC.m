e531a53f64e4faa445f4df63465fd8b8b75336b6
Jack <projectbyjx@gmail.com>
2023-08-06 15:42:15 -0400

    + minor mods

--- Geometric-Impedence-MPC/main_GMPIC.m
+++ Geometric-Impedence-MPC/main_GMPIC.m
@@ -21,10 +21,20 @@ validate.set_validatorLevel("all")  % [ all, none ]
 VIEW_BNDRY = [-1 1 -1 1 0 2];
 VIEW_ANGLE = [45 10];
 VIEW_DIMENSION = [400,400];
-% ---
-Exp_Title = "Const_F_EE";
-% Exp_Title = "Zero_F_EE";
+% --- 
+% - Simulation Params:
+FB_Ctrl_CONFIG = struct("Kp", 0.8, "Ki", 0.8, "Kd", 1);
+MODES_EE_F = ["Zero_F_EE","Const_F_EE","Dynamic_F_EE"];
+MODES_TRAJ = ["Steady","Const_dTheta","Dynamic_dTheta"];
 
+MODE = [MODES_EE_F(1), MODES_TRAJ(2)];
+Exp_Title = strcat(MODE(:));
+
+% - compute tag:
+FB_CTRL_TAG = sprintf("P[%s]I[%s]D[%s]",...
+    helper.float2str(FB_Ctrl_CONFIG.Kp),...
+    helper.float2str(FB_Ctrl_CONFIG.Ki),...
+    helper.float2str(FB_Ctrl_CONFIG.Kd));
 % --- 
 helper.endSection(AUTO_CLOSE);
 %% Initial g(0)) ===== ===== ===== ===== ===== ===== =====:
@@ -50,17 +60,25 @@ list_of_force_at_EE = zeros(6,N_end);
 FPS = 10;
 dT = 1/FPS; %s
 for i=2:N_end 
-    list_of_thetas(:,i) = [-2.6+pi/50*i, -pi/100*i, 0, 0, 0, 0, 0, 0 ]';
+    switch MODE(2)
+        case "Steady"
+            list_of_thetas(:,i) = [pi/2, -pi/2, 0, 0, 0, 0, 0, 0 ]';
+        case "Const_dTheta"
+            list_of_thetas(:,i) = [-2.6+pi/50*i, -pi/100*i, 0, 0, 0, 0, 0, 0 ]';
+        case "Dynamic_dTheta"
+            list_of_thetas(:,i) = [-2.6+2*pi*sin(i/100*2*pi), -pi/100*i, 0, 0, 0, 0, 0, 0 ]';
+    end
     % apply input constraints:
     list_of_thetas(:,i) = model.filter_angles(list_of_thetas(:,i));
     list_of_thetas(:,i) = model.joint_constraints(list_of_thetas(:,i));
     % inject disturbance at the EE:
-    if Exp_Title == "Zero_F_EE"
-        continue % do nothing
-    elseif Exp_Title == "Const_F_EE"
-        list_of_force_at_EE(:,i) = ones(6,1);
-    elseif Exp_Title == "Dynamic_F_EE"
-        list_of_force_at_EE(:,i) = ones(6,1)*sin(pi*i/10);
+    switch MODE(1)
+        case "Zero_F_EE"
+            continue % do nothing
+        case "Const_F_EE"
+            list_of_force_at_EE(:,i) = ones(6,1);
+        case "Dynamic_F_EE"
+            list_of_force_at_EE(:,i) = ones(6,1)*sin(pi*i/10);
     end
 end
 list_of_thetas(:,1)=list_of_thetas(:,2);
@@ -76,12 +94,11 @@ utils.plot3DBoundary(VIEW_BNDRY);
 model.plot_now();
 view(VIEW_ANGLE)
 axis equal;
-helper.saveFigure(VIEW_DIMENSION, DIR, "trajectory");
+helper.saveFigure(VIEW_DIMENSION, DIR, "reference_trajectory");
 
 
 %% compute trajectory after feedback linearization controller with dynamics:
 helper.logtitle("Compute Dynamics Controller")
-FB_Ctrl_CONFIG = struct("Kp", 0.8, "Ki", 0.8, "Kd", 1);
 data = model.dynamic_sim(list_of_thetas(:,1), list_of_thetas(:,2:end), ...
     list_of_force_at_EE, dT, FB_Ctrl_CONFIG);
 % compute fwd traj:
@@ -94,11 +111,7 @@ utils.plot3DBoundary(VIEW_BNDRY);
 model.plot_now();
 view(VIEW_ANGLE)
 axis equal;
-TAG = sprintf("P[%s]I[%s]D[%s]",...
-    helper.float2str(FB_Ctrl_CONFIG.Kp),...
-    helper.float2str(FB_Ctrl_CONFIG.Ki),...
-    helper.float2str(FB_Ctrl_CONFIG.Kd));
-file_name = strcat("trajectory_fb_controller_",TAG);
+file_name = strcat("trajectory_fb_controller_", FB_CTRL_TAG);
 helper.saveFigure(VIEW_DIMENSION, DIR, file_name);
 
 %% plot joints:
@@ -107,12 +120,12 @@ utils.plot_position_vs_t(...
     {spatial_frame_SE3_,spatial_frame_SE3_fb_}, ...
     ["ref", "PID"], ...
     dT);
-helper.saveFigure(VIEW_DIMENSION, DIR, strcat("EE_position",TAG));
+helper.saveFigure(VIEW_DIMENSION, DIR, strcat("EE_position_",FB_CTRL_TAG));
 
 %% animate:
 pause(0.00001)
 helper.logtitle("Animating ...")
-model.animate_with(data.theta, VIEW_DIMENSION, VIEW_BNDRY, VIEW_ANGLE, DIR, TAG, FPS);
+model.animate_with(data.theta, VIEW_DIMENSION, VIEW_BNDRY, VIEW_ANGLE, DIR, FB_CTRL_TAG, FPS);
 
 % --- 
 helper.endSection(AUTO_CLOSE);
